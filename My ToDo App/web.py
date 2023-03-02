cd69cec1a1cf948cd0f0d59393ebf4998f4d057d
Letizia <letizia.faulisi@gmail.com>
2023-03-02 11:38:32 +0100

    Implement the completing ToDo Item feature

--- My ToDo App/web.py
+++ My ToDo App/web.py
@@ -12,8 +12,12 @@ def add_todo():
 st.title("My ToDo app")
 st.subheader("This is an app to encrease your productivity")
 
-for todo in todos:
-    st.checkbox(todo)
+for index,todo in enumerate(todos):
+    checkbox = st.checkbox(todo, key=todo)
+    if checkbox:
+        todos.pop(index)
+        functions.write_todos(todos)
+        del st.session_state[todo]
+        st.experimental_rerun()
 
 st.text_input(label = "", placeholder = "Add a new ToDo", on_change=add_todo, key='new_todo')
-
